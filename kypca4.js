439d773df9a7cfffca10966f5859e03915b79142
kostch <74373321+kostch@users.noreply.github.com>
2021-01-12 22:36:30 +0300

    Курсовая работа
    Так, надо проверить какое значение в 28 строке и отличается ли оно от значения в 27 и в 26, и если все отлично, то в function Min(i) сделать let m = getRandomIntInclusive(a,b) ну и далее result.unshift(m)
    Дальше посмотрим

--- kypca4.js
+++ kypca4.js
@@ -1,20 +1,35 @@
 z = 0
-Get(z)
+let result = []
+Min(z)
 function Min(i){
     for(a = i; -100 <= a ; a--) {
         let b = a + 10
         console.log(getRandomIntInclusive(a, b))
-        let l = getRandomIntInclusive(a,b)
+        result.unshift(getRandomIntInclusive(a,b))
     }
-    Get(a)
 }
-
+C(0)
 function Get(i) {
     for(a = i; 100 > a ; a++) {
-        let b = a + 10
+        let b = a + '10'
         console.log(getRandomIntInclusive(a, b))
+        C(i)
     }
     Min(a)
+}
+
+function Las(i) {
+    a = i
+    a = parseInt(a);
+    let b = a + 10
+    let res = result.shift()
+    console.log(res + getRandomIntInclusive(a, b))
+    let f = getRandomIntInclusive(a,b)
+    console.log(getRandomIntInclusive(a,b), 'tets getrandom')
+    let fe = res + f
+    result.unshift(fe)
+    C(i)
+//    Min(a)
 }
 
 function getRandomIntInclusive(min, max) {
@@ -23,3 +38,15 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
+function C(i){
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Представляем что это крутилка которая, например, добавляет или убавляет обороты, поэтому пишем число либо положительное, либо отрицательное ', (answer) => {
+        // TODO: Log the answer in a database
+        rl.close();
+        console.log(`Результат регулирования: ${Las(answer)}`);
+    });
+}
